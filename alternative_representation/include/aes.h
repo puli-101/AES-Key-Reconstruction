99ba79075c9f36da69183df9d6f1ddbfeb79e42a
David Pulido <dpulidoc99@gmail.com>
2024-06-07 16:57:51 +0200

    reorg files

---
+++ alternative_representation/include/aes.h
@@ -0,0 +1,26 @@
+#ifndef __AES_H__
+#define __AES_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#define NB_BYTES 16
+#define ROUNDS 11
+#define NB_WORDS 4
+
+extern uint8_t sbox[256];
+
+extern uint8_t rcon[255];
+
+//substitution avec SBOX
+uint32_t sub(uint32_t n);
+
+//rotation d'un octet a gauche avec wrapping
+uint32_t rot(uint32_t n);
+
+//prints an AES key schedule of a specific number of rounds and words per round
+void print_schedule(uint32_t schedule[15][NB_WORDS], int rounds);
+
+//prints the alternative version of the representation of the key schedule
+void print_new_schedule(uint8_t s[ROUNDS][NB_BYTES]);
+
+#endif
